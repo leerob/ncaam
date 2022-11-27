476db03feac4f5f5bfcd28191f817949a53ea398
Lee Robinson <lrobinson2011@gmail.com>
2022-11-27 11:46:19 -0600

    Set up boilerplate.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    // Example: https://a.espncdn.com/i/teamlogos/ncaa/500/66.png
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'a.espncdn.com',
+        port: '',
+        pathname: '/i/teamlogos/ncaa/**',
+      },
+    ],
+    formats: ['image/avif', 'image/webp'],
+  },
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
