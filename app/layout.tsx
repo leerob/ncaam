449c18d9ba02dcfe3186c41c05461d51832ed0e2
Lee Robinson <lrobinson2011@gmail.com>
2023-01-10 21:39:33 -0600

    Add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,26 @@ export default function RootLayout({
       <body className="text-black dark:text-white bg-white dark:bg-black">
         {children}
       </body>
+      <footer className="text-gray-600 dark:text-gray-400 text-xs mx-auto text-center mt-2 mb-4">
+        {'Built using the '}
+        <a
+          href="https://espn.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="border-b border-gray-400 dark:border-gray-600 hover:border-gray-600 hover:dark:border-gray-400 hover:text-gray-800 hover:dark:text-gray-200 transition-all"
+        >
+          ESPN API
+        </a>
+        {', Next.js, and Vercel. '}
+        <a
+          href="https://github.com/leerob/ncaam"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="border-b border-gray-400 dark:border-gray-600 hover:border-gray-600 hover:dark:border-gray-400 hover:text-gray-800 hover:dark:text-gray-200 transition-all"
+        >
+          View the code.
+        </a>
+      </footer>
     </html>
   );
 }
