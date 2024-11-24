85a3cb50bcd342b6d64e0d22dca82c604db13504
Lee Robinson <me@leerob.io>
2024-11-24 08:48:09 -0600

    Attempt fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,6 @@ export const metadata = {
     template: '%s | NCCAM Scores & Schedules',
   },
   description: 'Like ESPN, but streamlined.',
-  icons: {
-    shortcut:
-      'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏀</text></svg>',
-  },
 };
 
 export default function RootLayout({
