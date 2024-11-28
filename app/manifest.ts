ceed3d6f816572f8820b519369cd6302961479a0
Lee Robinson <me@leerob.io>
2024-11-27 19:23:19 -0600

    Use theme color in root layout

--- app/manifest.ts
+++ app/manifest.ts
@@ -8,7 +8,6 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
-    theme_color: '#ffffff',
     icons: [
       {
         src: '/icon-192x192.png',
